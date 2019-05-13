1e741a7123817d2e8520cec381cdd3b974757304
Worker-Heaven <superstar.geekboy@gmail.com>
2019-05-13 17:52:15 +0800

    Complete landing page view

--- clientapp/views.py
+++ clientapp/views.py
@@ -3,10 +3,16 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.template import loader
 
+from .models import Client
+
 
 def index(request):
     template = loader.get_template('clientapp/index.html')
-    context = {}
+
+    clients = Client.objects.all()
+    context = {
+        'clients': clients,
+    }
 
     return HttpResponse(template.render(context, request))
 
@@ -14,28 +20,86 @@ def index(request):
 def client_new(request):
     template = loader.get_template('clientapp/client_new.html')
     context = {
-        'label': 'Save',
+        'label': 'Add',
     }
 
     if request.method == 'GET':
         return HttpResponse(template.render(context, request))
     else:
-        return HttpResponseRedirect(reverse('index'))
+        full_name = request.POST['full_name']
+        contact_name = request.POST['contact_name']
+        address = request.POST['address']
+        email_address = request.POST['email_address']
+        phone_number = request.POST['phone_number']
+
+        if len(full_name) > 0 and len(email_address) > 0 and len(phone_number) > 0:
+            client = Client(
+                full_name=full_name,
+                contact_name=contact_name,
+                address=address,
+                email_address=email_address,
+                phone_number=phone_number,
+            )
+
+            client.save()
+
+            return HttpResponseRedirect(reverse('index'))
+
+        else:
+            context = {
+                'label': 'Add',
+                'full_name': full_name,
+                'address': address,
+                'contact_name': contact_name,
+                'email_address': email_address,
+                'phone_number': phone_number,
+            }
+
+            return HttpResponse(template.render(context, request))
 
 
 def client_update(request, client_id):
     template = loader.get_template('clientapp/client_update.html')
+
+    clientInfo = Client.objects.get(pk=client_id)
     context = {
         'label': 'Update',
         'client_id': client_id,
-        'full_name': 'jackson',
-        'address': 'Fake Address',
-        'contact_name': 'Fake Contact Name',
-        'email_address': 'Fake Email Address',
-        'phone_number': 'Fake Phone Number',
+        'full_name': clientInfo.full_name,
+        'address': clientInfo.address,
+        'contact_name': clientInfo.contact_name,
+        'email_address': clientInfo.email_address,
+        'phone_number': clientInfo.phone_number,
     }
 
     if request.method == 'GET':
         return HttpResponse(template.render(context, request))
     else:
-        return HttpResponseRedirect(reverse('index'))
+        full_name = request.POST['full_name']
+        contact_name = request.POST['contact_name']
+        address = request.POST['address']
+        email_address = request.POST['email_address']
+        phone_number = request.POST['phone_number']
+
+        if len(full_name) > 0 and len(email_address) > 0 and len(phone_number) > 0:
+            clientInfo.full_name = full_name
+            clientInfo.address = address
+            clientInfo.contact_name = contact_name
+            clientInfo.email_address = email_address
+            clientInfo.phone_number = phone_number
+
+            clientInfo.save()
+
+            return HttpResponseRedirect(reverse('index'))
+
+        else:
+            context = {
+                'label': 'Add',
+                'full_name': full_name,
+                'address': address,
+                'contact_name': contact_name,
+                'email_address': email_address,
+                'phone_number': phone_number,
+            }
+
+            return HttpResponse(template.render(context, request))
